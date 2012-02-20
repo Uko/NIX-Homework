0faa3c404caf8694c80e2058a65ad3be5e9edbb0
Uko <uko@unikernel.net>
2012-02-20 18:10:58 +0200

    Finished spSubdirs function, implemented recursive copy for dirs only.

--- C/file-task.c
+++ C/file-task.c
@@ -23,27 +23,106 @@ void help()
   exit(EXIT_FAILURE);
 }
 
+int stringArrayOfSizeContainsString(char **array, int size, char *string)
+{
+  int i;
+  for(i=0; i<size; i++)
+    if(strcmp(array[i],string)==0)
+       return 1;
+  return 0;
+}
+
+int recursiveCopy(char *from, char *where, char *what, int file)
+{
+  if(file)
+  {
+    //implement file copy
+    printf("file: %s/%s\n",from,what);
+  }
+  else
+  {
+    char * newDirPath = malloc(snprintf(NULL, 0, "%s/%s", where, what) + 1);
+    sprintf(newDirPath, "%s/%s", where, what);
+    if(mkdir(newDirPath,S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH|S_IXOTH))
+    {
+      printf("Failed to copy directory: %s\n",newDirPath);
+      return 1;
+    }
+    DIR *thisDir;
+    struct dirent* dirEntry;
+    char * thisDirPath = malloc(snprintf(NULL, 0, "%s/%s", from, what) + 1);
+    sprintf(thisDirPath, "%s/%s", from, what);
+    if(thisDir = opendir(thisDirPath))
+    {
+      while(dirEntry = readdir(thisDir))
+      {
+	if( strcmp(dirEntry->d_name, ".") != 0 &&
+	    strcmp(dirEntry->d_name, "..") != 0 )
+	{
+	   recursiveCopy(thisDirPath, newDirPath, dirEntry->d_name, dirEntry->d_type==DT_REG);
+	}
+      }
+      closedir(thisDir);
+    }
+    return 0;
+  }
+}
+
 int cpSubdirs(char *src, char *dst)
 {
   DIR *srcDir;
   DIR *dstDir;
   struct dirent* dirEntry;
+  char **destDirSubdirs;
+  int destDirSubdirsThreshold = 10;
+  destDirSubdirs = malloc(destDirSubdirsThreshold*sizeof(char *));
+  int amountOFSubdirsInDstDir = 0;
   
-  srcDir = opendir(src);
-  dstDir = opendir(dst);
-  if(srcDir == NULL)
+
+  
+  if(dstDir = opendir(dst))
   {
-    printf("Cannot open directory: '%s'\n", src);
-    exit(EXIT_FAILURE);
+    while(dirEntry = readdir(dstDir))
+    {
+      if( dirEntry->d_type == DT_DIR &&
+	  strcmp(dirEntry->d_name, ".") != 0 &&
+	  strcmp(dirEntry->d_name, "..") != 0 )
+      {
+	if(amountOFSubdirsInDstDir==destDirSubdirsThreshold)
+	{
+	  destDirSubdirsThreshold*=2;
+	  destDirSubdirs = realloc(destDirSubdirs,destDirSubdirsThreshold);
+	}
+	destDirSubdirs[amountOFSubdirsInDstDir]=dirEntry->d_name;
+	amountOFSubdirsInDstDir++;
+      }
+    }
+    if(srcDir = opendir(src))
+    {
+      while(dirEntry = readdir(srcDir))
+      {
+	if( dirEntry->d_type == DT_DIR &&
+	    strcmp(dirEntry->d_name, ".") != 0 &&
+	    strcmp(dirEntry->d_name, "..") != 0 &&
+	    !stringArrayOfSizeContainsString(destDirSubdirs,
+					     amountOFSubdirsInDstDir,
+					     dirEntry->d_name))
+	{
+	  recursiveCopy(src,dst,dirEntry->d_name,0);
+	}
+      }
+    }
+    else
+    {
+      printf("Cannot open directory: '%s'\n", src);
+      return 1;
+    }
   }
-  if(dstDir == NULL)
+  else
   {
     printf("Cannot open directory: '%s'\n", dst);
-    exit(EXIT_FAILURE);
-  }
-
-
-  
+    return 1;
+  } 
 
   return 0;
 }
@@ -84,6 +163,6 @@ int main(int argc, char *argv[])
   }
   
 
-  printf("args left %i\n",globalArgs.numOfDirs);
+  cpSubdirs(globalArgs.directories[0],globalArgs.directories[1]);
   return 0;
 }
